4fa7441d92b7680950820fc7e9d3c433dcc1fb18
davidabutler92 <davidabutler92@yahoo.com>
2020-03-23 13:39:34 -0700

    add flesh out scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,16 @@ var add = function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}
 
-alert(add(number1, number2))
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-var substract = function()
+var division = function(number1, number2) {
+  return number1 / number2;
+};
+
+alert(subtract(number1, number2))
