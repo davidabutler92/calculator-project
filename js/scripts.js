3d35e1eb8d49d3a4eb750c2375de268ae9b10b65
davidabutler92 <davidabutler92@yahoo.com>
2020-03-25 16:45:57 -0700

    add fix typo

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,16 @@ var division = function(number1, number2) {
 //Everything below this is user interface (front-end) logic:
 
 $(document).ready(function() {
-  var number1 = parseInt($("add1").val());
-  var number2 = parseInt($("add2").val());
-  alert(add(number1, number2));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
 });
+
+
 
 
 
