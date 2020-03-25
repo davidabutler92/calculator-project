060ad8dc9c4931b3f69b6367a9fa5a140b20985d
davidabutler92 <davidabutler92@yahoo.com>
2020-03-25 16:14:16 -0700

    add front end frame work

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,6 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
 var subtract = function(number1, number2) {
   return number1 - number2;
 }
@@ -16,5 +13,3 @@ var multiply = function(number1, number2) {
 var division = function(number1, number2) {
   return number1 / number2;
 };
-
-alert(subtract(number1, number2))
