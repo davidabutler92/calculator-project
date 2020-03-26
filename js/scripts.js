0c698a418e83d322bf0abbea4427e45dbe71f7d9
davidabutler92 <davidabutler92@yahoo.com>
2020-03-25 18:22:15 -0700

    website has four functioning algorithms

--- js/scripts.js
+++ js/scripts.js
@@ -27,12 +27,27 @@ $(document).ready(function() {
     $("#output1").text(result);
   });
  
-
-  $("form#sub").submit(function(event) {
+   $("form#sub").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#sub1").val());
     var number2 = parseInt($("#sub2").val());
     var result = subtract(number1, number2);
     $("#output2").text(result);
+  });
+
+  $("form#times").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#times1").val());
+    var number2 = parseInt($("#times2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = division(number1, number2);
+    $("#output4").text(result);
   });
 });
